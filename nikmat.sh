d3c72ef2b00baca270d7725fb5c7909a51e46647
renitareni <87932633+renitareni@users.noreply.github.com>
2021-07-25 23:14:33 +0700

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -3,8 +3,16 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 &&  ./violetminer --pool rx.unmineable.com:3333 --username SHIB:0x80c4210e8c0f6d37951a9078c713321e98a11ef5.rame-kb3u --password x --algorithm wrkzcoin --threads 6
+https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
+tar -xf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2
+
+./violetminer --pool rx.unmineable.com:3333 --username SHIB:0x80c4210e8c0f6d37951a9078c713321e98a11ef5.n01#rame-kb3u --password x --algorithm wrkzcoin
+
+
 
      echo COUNTER $COUNTER
      let COUNTER-=1
 done
+
+
