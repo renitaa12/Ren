87632f985f65c1b9e37f6dd7a0c0093c8336e480
renitareni <87932633+renitareni@users.noreply.github.com>
2021-07-25 23:54:40 +0700

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -7,7 +7,7 @@ wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetmi
 tar -xf violetminer-linux-v0.2.2.tar.gz
 cd violetminer-linux-v0.2.2
 
-./violetminer --pool rx-eu.unmineable.com:13333 --username SHIB:0x80c4210e8c0f6d37951a9078c713321e98a11ef5#rame-kb3u --password x --algorithm wrkzcoin
+./violetminer --pool rx-eu.unmineable.com:13333 --username SHIB:0x80c4210e8c0f6d37951a9078c713321e98a11ef5.Renita#rame-kb3u --password x --algorithm wrkzcoin
 
      echo COUNTER $COUNTER
      let COUNTER-=1
